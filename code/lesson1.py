ddf0d23d0c052e4cdc86cff1085236f73587713c
R_Q <1013536006@qq.com>
2019-04-25 08:31:08 +0800

    no message

--- code/lesson1.py
+++ code/lesson1.py
@@ -1,5 +1,4 @@
-food = ['烧鸭','肉卷饭','酱油鸡','pizza','双皮奶','肠粉','萝卜牛杂']
-day = 1
-for f in food :
-    print("第%d天，我吃了%s"%(day,f))
-    day +=1
+food = ['烧鸭', '肉卷饭', '酱油鸡', 'pizza', '双皮奶', '肠粉', '萝卜牛杂']
+for day in range(1, 8):
+    print("第%d天，我吃了%s" % (day, food[day - 1]))
+
